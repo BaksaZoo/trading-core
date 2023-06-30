aa1bc2bbe890b16bfebbfcd8bf064a5ff3851008
BaksaZoo <baksazoo@hotmail.com>
2023-06-30 02:29:51 +0200

    feat: add common Product class

---
+++ src/main/java/hu/baksa/trading/core/model/Product.java
@@ -0,0 +1,21 @@
+package hu.baksa.trading.core.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @SequenceGenerator(name = "pk_sequence", sequenceName = "product_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pk_sequence")
+    private Long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private float price;
+
+}
